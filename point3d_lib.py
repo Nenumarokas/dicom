e18025dcb1e13a93c6fa3747b0e9c3d57773a59e
Nenumarokas <marius.pazera@gmail.com>
2024-11-24 22:32:01 +0200

    implemented skeleton perp. angle
    implemented skeleton perpendicular angle calculation to the nearest neighbour points.

--- point3d_lib.py
+++ point3d_lib.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 class Point:
-    def __init__(self, coordinates):
+    def __init__(self, coordinates: np.ndarray):
         self.coordinates: np.ndarray = coordinates
         self.nearby: list[Point] = []
         self.value: int = 0
-        self.angle: float = 0
+        self.angle: np.ndarray = np.array([-1, -1, -1])
         self.cross: bool = False
         self.end: bool = False
 
@@ -16,9 +16,9 @@ class Point:
         self.cross = len(self.nearby) > 2
         self.end = len(self.nearby) == 1
 
-    def get_surround_points(self, offsets) -> list[tuple]:
+    def get_surround_points(self, nearby_pixels: list) -> list[tuple]:
         cx, cy, cz = self.coordinates
-        return [(cx+o[0], cy+o[1], cz+o[2]) for o in offsets]
+        return [(cx+p[0], cy+p[1], cz+p[2]) for p in nearby_pixels]
     
     def is_cross_close(self, cross_distance: int, visited_points: list['Point'] = []) -> bool:
         if cross_distance == 0:
@@ -32,7 +32,7 @@ class Point:
             return point.is_cross_close(cross_distance-1, visited_points)
         return False
 
-    def remove_point(self):
+    def remove_point(self) -> None:
         for point in self.nearby:
             point.nearby.remove(self)
             point.check_state()
@@ -40,7 +40,8 @@ class Point:
         self.cross = False
         self.end = False
 
-    def distance_to_far_end(self, path: list['Point'] = [], distance: int = 0) -> tuple[int, list['Point']]:
+    def distance_to_far_end(self, path: list['Point'] = [], distance: int = 0)\
+            -> tuple[int, list['Point']]:
         path = path.copy()
         if self.end and distance > 0:
             return distance, path
@@ -56,7 +57,8 @@ class Point:
         distance_paths.sort(key = lambda x: x[0])        
         return distance_paths[-1]
     
-    def path_to_end(self, last_point: 'Point', path: list['Point'] = [], distance: int = 0):
+    def path_to_end(self, last_point: 'Point', path: list['Point'] = [], distance: int = 0)\
+            -> tuple[int, list['Point']]:
         path = path.copy()
         if self.end and self == last_point:
             return distance, path
@@ -72,13 +74,43 @@ class Point:
                 return result
         return -1, path
 
-    def calculate_angle(self) -> float:
-        if self.nearby != 2:
-            self.angle = 0
-            return -1
-        left_point = self.nearby[0].coordinates.copy()
-        right_point = self.nearby[1].coordinates.copy()
-        self.angle = 1
+    def calculate_top_vector(self) -> bool:        
+        if len(self.nearby) != 2:
+            self.angle = np.array([0, 0, 0])
+            return False
+        
+        point1 = self.nearby[0].coordinates.copy()
+        point2 = self.nearby[1].coordinates.copy()
+        diff_vector = point1 - point2
+        
+        if diff_vector[0] == 0 and diff_vector[1] == 0:
+            # if the plane is horizontal or impossible
+            self.angle = np.array([0, 0, 0])
+            return False
+        
+        # find most top-pointing point on a perpendicular plane
+        # u = (-ac, -bc, (a^2)+(b^2))
+        x_coord, y_coord, z_coord = diff_vector
+        up_vector = (-x_coord*z_coord, -y_coord*z_coord, x_coord**2+y_coord**2)
+        self.angle = np.array(up_vector).astype(float)
+        return True
+        
+    def get_neighbour_angle_average(self) -> bool:
+        if len(self.nearby) == 0:
+            return False
+        if self.is_angle_set():
+            return True
+        
+        average = np.array([0.0, 0.0, 0.0])
+        for point in self.nearby:
+            if not point.is_angle_set():
+                return False
+            average += point.angle
+        self.angle = average / len(self.nearby)
+        return True
+        
+    def is_angle_set(self):
+        return np.any(self.angle != 0) and np.any(self.angle != -1)
         
     def distance_to_point(self, another: 'Point') -> float:
         return np.linalg.norm(self.coordinates - another.coordinates)
@@ -88,3 +120,28 @@ class Point:
     
     def __repr__(self):
         return f'{self.coordinates} ({len(self.nearby)})'
+
+if __name__ == '__main__':
+    print('\n \n ')
+    point1 = Point(np.array([0, 4, 3]))
+    point2 = Point(np.array([0, 1.5, 2]))
+    point3 = Point(np.array([0, 1, 1]))
+    
+    point1.add_nearby(point2)
+    point2.add_nearby(point1)
+    point2.add_nearby(point3)
+    point3.add_nearby(point2)
+    
+    skeleton = [point1, point2, point3]
+    
+    for point in skeleton:
+        print('----')
+        print(point.calculate_top_vector())
+    
+    print('\n')
+    for point in skeleton:
+        print('----')
+        print(point.get_neighbour_angle_average())
+    
+    for point in skeleton:
+        print(point.angle)
