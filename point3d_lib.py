186b35aa6ac0e329de902d0f08a4887eff6c1420
Nenumarokas <marius.pazera@gmail.com>
2024-11-26 17:54:55 +0200

    added top normal vector
    added point top normal vector calculation

--- point3d_lib.py
+++ point3d_lib.py
@@ -1,11 +1,15 @@
 import numpy as np
+import itertools
+
+NEARBY_PIXEL_OFFSET = list(itertools.product([-1, 0, 1], repeat=3))
+NEARBY_PIXEL_OFFSET.remove((0, 0, 0))
 
 class Point:
     def __init__(self, coordinates: np.ndarray):
         self.coordinates: np.ndarray = coordinates
         self.nearby: list[Point] = []
         self.value: int = 0
-        self.angle: np.ndarray = np.array([-1, -1, -1])
+        self.top_normal: np.ndarray = np.array([0.0, 0.0, 0.0])
         self.cross: bool = False
         self.end: bool = False
 
@@ -16,9 +20,9 @@ class Point:
         self.cross = len(self.nearby) > 2
         self.end = len(self.nearby) == 1
 
-    def get_surround_points(self, nearby_pixels: list) -> list[tuple]:
+    def get_surrounding_points(self) -> list[tuple]:
         cx, cy, cz = self.coordinates
-        return [(cx+p[0], cy+p[1], cz+p[2]) for p in nearby_pixels]
+        return [(cx+p[0], cy+p[1], cz+p[2]) for p in NEARBY_PIXEL_OFFSET]
     
     def is_cross_close(self, cross_distance: int, visited_points: list['Point'] = []) -> bool:
         if cross_distance == 0:
@@ -74,43 +78,55 @@ class Point:
                 return result
         return -1, path
 
-    def calculate_top_vector(self) -> bool:        
+    def calculate_top_normal(self) -> bool:
+        """
+        p - current point coords
+        p1 - neighbour 1 coords
+        p2 - neighbour 2 coords
+        n - plane normal
+        d - where the plane intersects with the vertical origin (0, 0, z)
+        v - current point coords to the plane intersection
+        np - most top-facing vector on a plane of an artery cross section centered on a skeleton point
+        """
         if len(self.nearby) != 2:
-            self.angle = np.array([0, 0, 0])
             return False
         
         point1 = self.nearby[0].coordinates.copy()
         point2 = self.nearby[1].coordinates.copy()
-        diff_vector = point1 - point2
+        diff_vector = (point1 - point2).astype(float)
         
         if diff_vector[0] == 0 and diff_vector[1] == 0:
-            # if the plane is horizontal or impossible
-            self.angle = np.array([0, 0, 0])
+            # if the plane is horizontal or non-existant
             return False
         
-        # find most top-pointing point on a perpendicular plane
-        # u = (-ac, -bc, (a^2)+(b^2))
-        x_coord, y_coord, z_coord = diff_vector
-        up_vector = (-x_coord*z_coord, -y_coord*z_coord, x_coord**2+y_coord**2)
-        self.angle = np.array(up_vector).astype(float)
+        # n = (p2-p1) / |p2-p1|
+        plane_normal = diff_vector / np.linalg.norm(diff_vector)
+        # d = |n·p| / n.z
+        vertical_intersection = np.dot(plane_normal, self.coordinates) / plane_normal[2]
+        # v = (0, 0, d) - p
+        vertical_point = np.array([0, 0, vertical_intersection]) - self.coordinates
+        vertical_point = vertical_point if vertical_intersection > 0 else -vertical_point
+        # np = (v / |v|) + p
+        self.top_normal = (vertical_point / np.linalg.norm(vertical_point)) + self.coordinates
+            
         return True
         
-    def get_neighbour_angle_average(self) -> bool:
+    def get_neighbour_top_normal_average(self) -> bool:
         if len(self.nearby) == 0:
             return False
-        if self.is_angle_set():
+        if self.is_top_normal_set():
             return True
         
         average = np.array([0.0, 0.0, 0.0])
         for point in self.nearby:
-            if not point.is_angle_set():
+            if not point.is_top_normal_set():
                 return False
-            average += point.angle
-        self.angle = average / len(self.nearby)
+            average += point.top_normal - point.coordinates
+        self.top_normal = average / len(self.nearby) + self.coordinates
         return True
         
-    def is_angle_set(self):
-        return np.any(self.angle != 0) and np.any(self.angle != -1)
+    def is_top_normal_set(self):
+        return np.any(self.top_normal != 0) and np.any(self.top_normal != -1)
         
     def distance_to_point(self, another: 'Point') -> float:
         return np.linalg.norm(self.coordinates - another.coordinates)
@@ -123,25 +139,35 @@ class Point:
 
 if __name__ == '__main__':
     print('\n \n ')
-    point1 = Point(np.array([0, 4, 3]))
-    point2 = Point(np.array([0, 1.5, 2]))
-    point3 = Point(np.array([0, 1, 1]))
+    point1 = Point(np.array([0, 1, 2]))
+    point2 = Point(np.array([1, 1, 3]))
+    point3 = Point(np.array([1, 2.5, 4]))
+    point4 = Point(np.array([2, 3, 4]))
+    point5 = Point(np.array([3, 4, 3]))
     
     point1.add_nearby(point2)
+    
     point2.add_nearby(point1)
     point2.add_nearby(point3)
-    point3.add_nearby(point2)
     
-    skeleton = [point1, point2, point3]
+    point3.add_nearby(point2)
+    point3.add_nearby(point4)
+    
+    point4.add_nearby(point3)
+    point4.add_nearby(point5)
+    
+    point5.add_nearby(point4)
+    
+    
+    skeleton = [point1, point2, point3, point4, point5]
     
     for point in skeleton:
         print('----')
-        print(point.calculate_top_vector())
+        point.calculate_top_normal()
     
     print('\n')
     for point in skeleton:
-        print('----')
-        print(point.get_neighbour_angle_average())
+        point.get_neighbour_top_normal_average()
     
-    for point in skeleton:
-        print(point.angle)
+    for i, point in enumerate(skeleton):
+        print(tuple([round(i, 2) for i in point.top_normal]))
